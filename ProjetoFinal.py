57306ce107be5c212324433487c8f4cf7ba8271a
Rodrigo Augusto Sarmento <7rodrigo.sarmento@gmail.com>
2018-06-10 15:48:04 -0300

    Update ProjetoFinal.py

--- ProjetoFinal.py
+++ ProjetoFinal.py
@@ -1,21 +1,50 @@
+#	----	Observações
+#The variable neighborhood descent (VND) method is obtained if a change of neighborhoods is 
+#performed in a deterministic way. In the descriptions of its algorithms, we assume that an 
+#initial solution x is given. Most local search heuristics in their descent phase use very few 
+#neighbourhoods. The final solution should be a local minimum with respect to all k m 
+#a x {\displaystyle k_{max}} k_{{max}} neighbourhoods; hence the chances to reach a global 
+#one are larger when using VND than with a single neighbourhood structure. 	
+		
+#Function VNS (x, kmax, tmax );
+# 1: repeat
+# 2:    k ← 1;
+# 3:    repeat
+# 4:       x' ←Shake(x, k) /* Shaking */;
+# 5:       x'' ← FirstImprovement(x' ) /* Local search */;
+# 6:       x ←NeighbourhoodChange(x, x'', k) /* Change neighbourhood */;
+# 7:    until k = k_max ;
+# 8:    t ←CpuTime()
+# 9: until t > t_max ;
+
+#Algoritmo
+#1) Initialize the result as {}
+#2) Consider a set of all edges in given graph.  Let the set be E.
+#3) Do following while E is not empty
+#...a) Pick an arbitrary edge (u, v) from set E and add 'u' and 'v' to result
+#...b) Remove all edges from E which are either incident on u or v.
+#4) Return result 
+#	-------------------
+
+#	----	Bibliotecas
 #Para realizar a escolha aleatória de uma aresta
 import random
 
+#	----	Conjunto de vértices
 #Set de vértices que abrange a menor quantidade de arestas possível
 conj_vertices = set()
-#conj_vertices = []
 
-#Classe aresta
+#	----	Classe aresta
 class aresta:
      def __init__(self, u, v):
          self.u = u
          self.v = v
 
-#Conjunto que contém todas as arestas         
+#	-----	Conjunto que contém todas as arestas         
 conj_arestas = []
 
 
-#abrearquivo para arquivos que possuem separação por espaço
+#	-----	Função para abrir arquivos e criar o conjunto de arestas
 def abreArquivo():
 	txt = open('frb30-15-mis/frb30-15-1 copy.mis','r').readlines()
 	array = []
@@ -34,6 +63,7 @@ def abreArquivo():
 	return array, qtd_vertices, qtd_arestas
 
 
+#	-----	Função para printar as arestas durante os testes
 def print_arestas():
 	print(" ------------------------------ \n")
 	print("Quantidade de arestas:",len(conj_arestas))
@@ -45,37 +75,9 @@ def print_arestas():
 	return	
 	
 
-
-    #The variable neighborhood descent (VND) method is obtained if a change of neighborhoods is 
-    #performed in a deterministic way. In the descriptions of its algorithms, we assume that an 
-    #initial solution x is given. Most local search heuristics in their descent phase use very few 
-    #neighbourhoods. The final solution should be a local minimum with respect to all k m 
-    #a x {\displaystyle k_{max}} k_{{max}} neighbourhoods; hence the chances to reach a global 
-    #one are larger when using VND than with a single neighbourhood structure. 	
-	
-	
-	#Function VNS (x, kmax, tmax );
-	# 1: repeat
-	# 2:    k ← 1;
-	# 3:    repeat
-	# 4:       x' ←Shake(x, k) /* Shaking */;
-	# 5:       x'' ← FirstImprovement(x' ) /* Local search */;
-	# 6:       x ←NeighbourhoodChange(x, x'', k) /* Change neighbourhood */;
-	# 7:    until k = k_max ;
-	# 8:    t ←CpuTime()
-	# 9: until t > t_max ;
-	
-
-def vertex_cover():
-	
-	#Utilizando lista
-	#conj_vertices.append(int(conj_arestas[0].u))
-	#conj_vertices.append(int(conj_arestas[0].v))
-	
-	#Utilizando set
-	#Utilizo "char" mesmo, devido ao fato de conferir "in"
-		
-	#Enquanto existirem arestas
+#	-----	Função que excluí arestas e adiciona vértices em um conjunto
+def vertex_cover():	
+	#Enquanto existirem arestas x
 	while conj_arestas:
 		#Adiciono arbitrariamente uma aresta
 		aux = random.choice(conj_arestas) 
@@ -101,23 +103,11 @@ def vertex_cover():
 						
 	return	
 	
-	
+#	-----	main
 def main():
 	elementos, qtd_vertices, qtd_arestas = abreArquivo()
 	print("Quantidade inicial de vértices: \n",qtd_vertices)
 	
-	
-	#Trabalhando com a classe conjunto de arestas
-	#print(conj_arestas[1].u,conj_arestas[1].v)
-	
-	#Algoritmo
-	#1) Initialize the result as {}
-	#2) Consider a set of all edges in given graph.  Let the set be E.
-	#3) Do following while E is not empty
-	#...a) Pick an arbitrary edge (u, v) from set E and add 'u' and 'v' to result
-	#...b) Remove all edges from E which are either incident on u or v.
-	#4) Return result 
-
 	print("Inicialmente temos:",len(conj_arestas),"arestas\n")
 	print_arestas()
 
